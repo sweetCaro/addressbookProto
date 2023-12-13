4d71bd3d768d7401adf72900740b10b0f80eaffa
sweetCaro <jp2020213614@qmul.ac.uk>
2023-12-14 00:10:39 +0800

    Initial commit

---
+++ src/test/java/com/example/protos/AddressBookTest.java
@@ -0,0 +1,18 @@
+package com.example.protos;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+/**
+ * Unit test for simple App.
+ */
+class AddressBookTest {
+    /**
+     * Rigorous Test.
+     */
+    @Test
+    void testApp() {
+        assertEquals(1, 1);
+    }
+}
